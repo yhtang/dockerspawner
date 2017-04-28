39e4e629f7134c71de71298674d4367ac077481e
Yu-Hang Tang <yuhang_tang@brown.edu>
2017-04-28 11:23:17 -0400

    include OwncloudUserSpawner to __init__

--- dockerspawner/__init__.py
+++ dockerspawner/__init__.py
@@ -1,5 +1,6 @@
 from ._version import __version__
 from .dockerspawner import DockerSpawner
 from .systemuserspawner import SystemUserSpawner
+from .ownclouduserspawner import OwncloudUserSpawner
 
-__all__ = ['__version__', 'DockerSpawner', 'SystemUserSpawner']
+__all__ = ['__version__', 'DockerSpawner', 'SystemUserSpawner', 'OwncloudUserSpawner']
