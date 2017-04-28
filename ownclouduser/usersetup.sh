eb03e6b3b1391e3f26645a02adfd114f9232d475
Yu-Hang Tang <yuhang_tang@brown.edu>
2017-04-28 11:45:58 -0400

    gracefully handling duplicate username

--- ownclouduser/usersetup.sh
+++ ownclouduser/usersetup.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 set -e
-if getent passwd $USER > /dev/null ; then
-  echo "$USER exists"
-else
-  echo "Creating user $USER"
-  useradd -u 9999 -s $SHELL $USER
-fi
+export UNIQUE_USER=$USER
+while getent passwd $UNIQUE_USER > /dev/null ; do
+  echo "$UNIQUE_USER exists"
+  export UNIQUE_USER=$USER$RANDOM
+  echo "Trying $UNIQUE_USER"
+done
 
-mkdir /home/$USER
-chown $USER:$USER /home/$USER
+echo "Creating user $UNIQUE_USER"
+useradd -u 9999 -s $SHELL --home-dir=/home/$UNIQUE_USER --create-home $UNIQUE_USER
+# chown $UNIQUE_USER:$UNIQUE_USER /home/$UNIQUE_USER
+
 # TODO: install davfs2 and mount owncloud
-
 
 notebook_arg=""
 if [ -n "${NOTEBOOK_DIR:+x}" ]
@@ -18,7 +19,7 @@ then
     notebook_arg="--notebook-dir=${NOTEBOOK_DIR}"
 fi
 
-sudo -E PATH="${CONDA_DIR}/bin:$PATH" -u $USER $CONDA_DIR/bin/jupyterhub-singleuser \
+sudo -E PATH="${CONDA_DIR}/bin:$PATH" -u $UNIQUE_USER $CONDA_DIR/bin/jupyterhub-singleuser \
   --port=8888 \
   --ip=0.0.0.0 \
   --user=$JPY_USER \
