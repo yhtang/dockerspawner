84481d35c4dabc41e54cd9fbf4ee7459235704f0
Yu-Hang Tang <yuhang_tang@brown.edu>
2017-04-29 15:16:51 -0400

    test add additional options

--- dockerspawner/ownclouduserspawner.py
+++ dockerspawner/ownclouduserspawner.py
@@ -1,4 +1,5 @@
 import pwd
+import shlex
 
 from dockerspawner import DockerSpawner
 from textwrap import dedent
@@ -10,6 +11,43 @@ from traitlets import (
 class OwncloudUserSpawner(DockerSpawner):
 
     container_image = Unicode("jupyterhub/ownclouduser", config=True)
+
+    def _options_form_default(self):
+        default_env = "YOURNAME=%s\n" % self.user.name
+        return """
+        <label for="args">Extra notebook CLI arguments</label>
+        <input name="args" placeholder="e.g. --debug"></input>
+        <label for="env">Environment variables (one per line)</label>
+        <textarea name="env">{env}</textarea>
+        """.format(env=default_env)
+    
+    def options_from_form(self, formdata):
+        options = {}
+        options['env'] = env = {}
+        
+        env_lines = formdata.get('env', [''])
+        for line in env_lines[0].splitlines():
+            if line:
+                key, value = line.split('=', 1)
+                env[key.strip()] = value.strip()
+        
+        arg_s = formdata.get('args', [''])[0].strip()
+        if arg_s:
+            options['argv'] = shlex.split(arg_s)
+        return options
+    
+    def get_args(self):
+        """Return arguments to pass to the notebook server"""
+        argv = super().get_args()
+        if self.user_options.get('argv'):
+            argv.extend(self.user_options['argv'])
+        return argv
+    
+    def get_env(self):
+        env = super().get_env()
+        if self.user_options.get('env'):
+            env.update(self.user_options['env'])
+        return env
 
     image_homedir_format_string = Unicode(
         "/home/{username}",
