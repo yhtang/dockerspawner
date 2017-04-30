084221d08987296bb7256a3332a34540095a01c6
Yu-Hang Tang <yuhang_tang@brown.edu>
2017-04-29 22:45:54 -0400

    routine

--- ownclouduser/usersetup.sh
+++ ownclouduser/usersetup.sh
@@ -9,18 +9,25 @@ else
 fi
 
 if [ ! -d /home/$USER ]; then
+  echo "Creating home folder for $USER at /home"
   mkdir /home/$USER
 fi
-chown -R $USER:$USER /home/$USER
+echo "Set ownership of /home/$USER to $USER:$USER"
+chown $USER:$USER /home/$USER
 
 # use davfs2 to mount ownCloud
+echo "Configure /etc/davfs2 ownership"
 chown -R root:root /etc/davfs2
 chmod 700 /etc/davfs2
 chmod 600 /etc/davfs2/*
 CLOUD_DIR=/cloud
+echo "$(whoami)"
+echo "Unmount /cloud"
 umount /cloud
+echo "Mount /cloud"
 mount -t davfs https://tangshan.cosx-isinx.org/owncloud/remote.php/webdav -o user,rw,auto,uid=$USER,gid=$USER $CLOUD_DIR
 
+echo "Launch notebook"
 sudo -E -u $USER jupyterhub-singleuser \
   --port=8888 \
   --ip=0.0.0.0 \
